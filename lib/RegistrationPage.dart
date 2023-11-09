1451b23e9f1b8be01c688990204dff53895486bd
Zarshedjon Nasimov <2110058@newuu.uz>
2023-11-09 09:16:44 +0500

    edited Padding values in RegistrationPage.dart

--- lib/RegistrationPage.dart
+++ lib/RegistrationPage.dart
@@ -27,107 +27,112 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(78.0),
-              child: Text(
-                'Registration',
-                style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'First Name',
-                  prefixIcon: const Icon(
-                    Icons.person_2_rounded,
-                    color: Colors.green,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 70.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    'Registration',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Last Name',
-                  prefixIcon: const Icon(
-                    Icons.person_2_rounded,
-                    color: Colors.green,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Contact Number',
-                  prefixIcon: const Icon(
-                    Icons.phone,
-                    color: Colors.green,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Email Address',
-                  prefixIcon: const Icon(
-                    Icons.mail,
-                    color: Colors.green,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: InkWell(
-                onTap: () => _selectDate(context),
-                child: InputDecorator(
-                  decoration: InputDecoration(
-                    hintText: 'Date of Birth',
-                    prefixIcon: const Icon(
-                      Icons.calendar_today,
-                      color: Colors.green,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'First Name',
+                      prefixIcon: const Icon(
+                        Icons.person_2_rounded,
+                        color: Colors.green,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
                   ),
-                  child: _selectedDate != null
-                      ? Text(
-                          "${_selectedDate!.toLocal()}".split(' ')[0],
-                          style: const TextStyle(fontSize: 16),
-                        )
-                      : const Text(
-                          'Select Date',
-                          style: TextStyle(fontSize: 16),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Last Name',
+                      prefixIcon: const Icon(
+                        Icons.person_2_rounded,
+                        color: Colors.green,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Contact Number',
+                      prefixIcon: const Icon(
+                        Icons.phone,
+                        color: Colors.green,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email Address',
+                      prefixIcon: const Icon(
+                        Icons.mail,
+                        color: Colors.green,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: InkWell(
+                    onTap: () => _selectDate(context),
+                    child: InputDecorator(
+                      decoration: InputDecoration(
+                        hintText: 'Date of Birth',
+                        prefixIcon: const Icon(
+                          Icons.calendar_today,
+                          color: Colors.green,
                         ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: _selectedDate != null
+                          ? Text(
+                              "${_selectedDate!.toLocal()}".split(' ')[0],
+                              style: const TextStyle(fontSize: 16),
+                            )
+                          : const Text(
+                              'Select Date',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
