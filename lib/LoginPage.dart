4c832845f56c37e1e9edfa7d17424e1ad25f88e8
Adil Raziev <2110103@newuu.uz>
2023-11-15 10:19:42 +0500

    updated LoginPage.dart: - Added Login Button - Added TextButton to move to Sign Up Page - Adjusted Page Paddings

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hotel_reservation_system/RegistrationPage.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -16,7 +17,7 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Center(
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 90.0),
+              padding: const EdgeInsets.symmetric(vertical: 150.0),
               child: Column(
                 children: [
                   Padding(
@@ -25,7 +26,7 @@ class _LoginPageState extends State<LoginPage> {
                       'Login Page',
                       style: TextStyle(
                           color: Colors.green,
-                          fontSize: 26,
+                          fontSize: 30,
                           fontWeight: FontWeight.bold),
                     ),
                   ),
@@ -45,7 +46,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                     child: TextField(
                       obscureText: true,
                       decoration: InputDecoration(
@@ -55,6 +56,61 @@ class _LoginPageState extends State<LoginPage> {
                           color: Colors.green,
                         ),
                         border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Don\'t have an account?',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(width: 5.0),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) =>  RegisterPage()),
+                            );
+                          },
+                          style: ButtonStyle(
+                            overlayColor: MaterialStateProperty.all(Colors.transparent),       // To remove after effects
+                          ),
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 13.0, horizontal: 30.0),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        side: BorderSide(color: Colors.green, width: 2),
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20),
                         ),
                       ),
